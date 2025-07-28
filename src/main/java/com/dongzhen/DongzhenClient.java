a6671706ed80e43372c5683e57dd34b48b8fc099
dongzhenmao <dongzhenmao@gmail.com>
2025-07-28 22:43:00 +0800

    Initial commit

---
+++ src/main/java/com/dongzhen/DongzhenClient.java
@@ -0,0 +1,10 @@
+package com.dongzhen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DongzhenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
